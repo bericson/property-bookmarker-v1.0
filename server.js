f83c8c4274da2d3cc6e6db9e0957cb639ef6f53b
benjig-win\benihana <ben.ericson@s1commerce.com>
2017-01-29 23:36:57 -0700

    Establish file/folder structure, continue getting bootstrapped; initial work styling elements w/ Bootstrap

--- server.js
+++ server.js
@@ -1,4 +1,12 @@
 var express = require('express');
 var app = express();
-app.use('/', express.static('./'));
+app.use(express.static('public'))
+//app.use('/', express.static('./'));
 app.listen(8080);
+
+// prepare server
+// app.use('/api', api); // redirect API calls
+// app.use('/', express.static(__dirname + '/www')); // redirect root
+// app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); // redirect bootstrap JS
+// app.use('/js', express.static(__dirname + '/node_modules/jquery/dist')); // redirect JS jQuery
+// app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css')); // redirect CSS bootstrap
